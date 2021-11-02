04988cb2f278a49092677e863d7b5d1d0c308967
Дмитрий Покладов <PokladovDV@gmail.com>
2021-11-02 15:15:37 +0300

    Регистрация

--- src/components/userRegister.vue
+++ src/components/userRegister.vue
@@ -9,6 +9,14 @@
       outlined
     />
     <v-text-field
+      v-model="user.login"
+      label="Логин"
+      placeholder="Обязательно"
+      :rules="[rules.required]"
+      outlined
+    >
+    </v-text-field>
+    <v-text-field
       v-model="user.password"
       type="password"
       autocomplete="new-password"
@@ -27,34 +35,37 @@
     />
     <v-text-field v-model="user.firstname" label="Имя" outlined />
     <v-text-field v-model="user.lastname" label="Фамилия" outlined />
-    <v-text-field v-model="user.phone" label="Телефон" outlined />
+    <v-text-field v-model="user.phone_number" label="Телефон" outlined />
     <v-text-field v-model="user.address.city" label="Город" outlined />
     <v-text-field v-model="user.address.street" label="Улица" outlined />
     <v-text-field v-model="user.address.build" label="Строение" outlined />
     <v-text-field v-model="user.address.apartment" label="Квартира" outlined />
-    <v-text-field v-model="user.birthday" label="День рождения" outlined />
+    <v-text-field v-model="user.birth_date" label="День рождения" outlined />
     <v-btn block type="submit">Зарегистрироваться</v-btn>
   </v-form>
 </template>
 
 <script>
+import user from "@/view/user";
+
 export default {
   name: "userRegister",
   data() {
     return {
       user: {
         mail: "",
+        login: "",
         password: "",
         firstname: "",
         lastname: "",
-        phone: "",
+        phone_number: "",
         address: {
           city: "",
           street: "",
           build: "",
           apartment: "",
         },
-        birthday: "",
+        birth_date: "",
       },
       rules: {
         required: (val) => val !== "" || "Обязательное поле",
@@ -69,8 +80,28 @@ export default {
   methods: {
     register() {
       if (this.$refs.form.validate()) {
-        console.log("регичтрация");
+        this.$http
+          .post("/reg/user", this.user)
+          .then(() => this.clear_data())
+          .then(() => this.$router.push({ path: "/login" }));
       }
+    },
+    clear_data() {
+      this.user = {
+        mail: "",
+        login: "",
+        password: "",
+        firstname: "",
+        lastname: "",
+        phone_number: "",
+        address: {
+          city: "",
+          street: "",
+          build: "",
+          apartment: "",
+        },
+        birth_date: "",
+      };
     },
   },
 };
