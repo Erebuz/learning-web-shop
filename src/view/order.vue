3055541f1e6c39181d27bf0446e435f79ac0ebf1
Дмитрий Покладов <PokladovDV@gmail.com>
2021-11-18 15:49:22 +0300

    Оформление

--- src/view/order.vue
+++ src/view/order.vue
@@ -1,16 +1,21 @@
 <template>
-  <v-container> Оформление заказа {{ par }} {{ summ }} </v-container>
+  <v-container>
+    <v-col cols="8" offset="2">
+      <v-card>
+        <v-card-title>Оформление заказа</v-card-title>
+        <v-card-text
+          >Вы заказали товаров на сумму: <b>{{ summ }} руб.</b> <br />Введите
+          номер телефона, чтобы наш менеджер мог с вами связаться</v-card-text
+        >
+      </v-card>
+    </v-col>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "order",
   props: ["summ"],
-  computed: {
-    par() {
-      return this.$route.params;
-    },
-  },
 };
 </script>
 
